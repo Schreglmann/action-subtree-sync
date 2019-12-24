ee19ad5e86a4e89ed0e3fbb844629e6095fb7827
Schreglmann <georgschreglmann@gmx.at>
2019-12-24 12:40:39 +0100

    Update entrypoint.sh

--- resources/root/entrypoint.sh
+++ resources/root/entrypoint.sh
@@ -4,16 +4,20 @@
 ssh-keyscan github.com > /root/.ssh/known_hosts
 
 # Set ssh key for subtree
-echo "${INPUT_SUBTREE_DEPLOY_KEY}" >> /root/.ssh/subtree
-chmod 0600 /root/.ssh/subtree
+echo "${INPUT_REPO_KEY}" >> /root/.ssh/repo_key
+chmod 0600 /root/.ssh/repo_key
+
+echo "${INPUT_DEPLOY_KEY}" >> /root/.ssh/deploy_key
+chmod 0600 /root/.ssh/deploy_key
 
 eval $(ssh-agent -s)
 
-ssh-add /root/.ssh/subtree
+ssh-add /root/.ssh/deploy_key
+ssh-add /root/.ssh/repo_key
 
 # Get subtree repository into split directory
-git clone git@github.com:"${INPUT_MAINREPO}".git /tmp/split
+git clone git@github.com:"${INPUT_MAIN_REPO}".git /tmp/split
 
 cd /tmp/split
 
-git subtree push --prefix web git@github.com:"${INPUT_SUBTREEREPO}".git master
+git subtree push --prefix web "${INPUT_DOKKU_REPO}" master
