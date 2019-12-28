b931395606031c5f6c5bf254f484333e6e4c2c61
Schreglmann <georgschreglmann@gmx.at>
2019-12-28 15:11:36 +0100

    Update entrypoint.sh

--- resources/root/entrypoint.sh
+++ resources/root/entrypoint.sh
@@ -1,22 +1,28 @@
 #!/bin/sh -l
 
-ssh-keyscan -t rsa "${INPUT_DOKKU_REPO}" >> /root/.ssh/known_hosts
-ssh-keyscan github.com >> /root/.ssh/known_hosts
 
+ssh-keyscan github.com > /root/.ssh/known_hosts
 chmod 600 /root/.ssh/known_hosts
 
-echo "${INPUT_REPO_KEY}" >> /root/.ssh/repo_key
+echo "${INPUT_REPO_KEY}" > /root/.ssh/repo_key
 chmod 0600 /root/.ssh/repo_key
-
-echo "${INPUT_DEPLOY_KEY}" >> /root/.ssh/deploy_key
-chmod 0600 /root/.ssh/deploy_key
 
 eval $(ssh-agent)
 
-ssh-add /root/.ssh/deploy_key
 ssh-add /root/.ssh/repo_key
 
 git clone git@github.com:"${INPUT_MAIN_REPO}".git /tmp/split
+
+
+
+
+
+ssh-keyscan -t rsa "${INPUT_DOKKU_REPO}" > /root/.ssh/known_hosts
+
+echo "${INPUT_DEPLOY_KEY}" > /root/.ssh/deploy_key
+chmod 0600 /root/.ssh/deploy_key
+ssh-add /root/.ssh/deploy_key
+
 
 cd /tmp/split
 
